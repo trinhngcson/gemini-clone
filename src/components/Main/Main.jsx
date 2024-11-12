2ec78d7dad58cc38058c1b969768a5395d4e7606
Trịnh Ngọc Sơn <77819889+trinhngcson@users.noreply.github.com>
2024-11-12 21:41:39 +0700

    UI of Gemini

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,90 @@
+import React from "react";
+import { assets } from "../../assets/assets";
+
+const Main = () => {
+  return (
+    <div className="flex-1 relative min-h-screen pb-[15vh]">
+      <div className="flex items-center justify-between text-xl p-5 text-gray-700">
+        <p>Gemini</p>
+        <img className="w-10 rounded-full" src={assets.user_icon} alt="" />
+      </div>
+      <div className="max-w-4xl m-auto">
+        <div className="my-12 text-6xl text-gray-400 font-medium p-5">
+          <p>
+            <span className="font-bold bg-gradient-to-r from-blue-500 via-purple-500 to-red-500 text-transparent bg-clip-text">
+              Hello, Dev.
+            </span>
+          </p>
+          <p>How can I help you today?</p>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-4 p-5">
+          <div className="relative card h-48 p-4 rounded-lg cursor-pointe bg-gray-100 hover:bg-gray-300">
+            <p className="text-gray-700 text-lg">
+              Suggest beautiful places to see on an upcoming road trip
+            </p>
+            <img
+              className="absolute w-9 p-1 bg-white rounded-2xl bottom-2 right-2"
+              src={assets.compass_icon}
+            />
+          </div>
+          <div className="relative card h-48 p-4 rounded-lg cursor-pointe bg-gray-100 hover:bg-gray-300">
+            <p className="text-gray-700 text-lg">
+              Briefly summarize this concept: urban planning
+            </p>
+            <img
+              className="absolute w-9 p-1 bg-white rounded-2xl bottom-2 right-2"
+              src={assets.bulb_icon}
+            />
+          </div>
+          <div className="relative card h-48 p-4 rounded-lg cursor-pointe bg-gray-100 hover:bg-gray-300">
+            <p className="text-gray-700 text-lg">
+              Brainstorm team bonding activities for our work retreat
+            </p>
+            <img
+              className="absolute w-9 p-1 bg-white rounded-2xl bottom-2 right-2"
+              src={assets.message_icon}
+            />
+          </div>
+          <div className="relative card h-48 p-4 rounded-lg cursor-pointe bg-gray-100 hover:bg-gray-300">
+            <p className="text-gray-700 text-lg">
+              Tell me about React js and React native
+            </p>
+            <img
+              className="absolute w-9 p-1 bg-white rounded-2xl bottom-2 right-2"
+              src={assets.code_icon}
+            />
+          </div>
+        </div>
+
+        <div className="absolute w-full bottom-8 md:bottom-20 max-w-4xl m-auto px-5">
+          <div className="py-1 md:py-2 px-2 md:px-5 flex items-center justify-between gap-5 bg-gray-100 rounded-full">
+            <input
+              type="text"
+              placeholder="Ask Gemini"
+              className="flex-none w-36 md:flex-1 bg-transparent border-none outline-none p-2 text-lg"
+            />
+            <div className="gap-1 md:gap-4 flex items-center ">
+              <img
+                className="w-5 md:w-6 cursor-pointer"
+                src={assets.gallery_icon}
+                alt=""
+              />
+              <img
+                className="w-5 md:w-6 cursor-pointer"
+                src={assets.mic_icon}
+                alt=""
+              />
+              <img
+                className="w-5 md:w-6 cursor-pointer"
+                src={assets.send_icon}
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,12 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { assets } from "../../assets/assets";
 const Sidebar = () => {
+  const [extended, setExtended] = useState(false);
   return (
-    <div className="">
+    <div className="hidden md:inline-flex h-screen flex-col justify-between bg-blue-100 py-6 px-5">
       <div className="">
-        <img className="" src={assets.menu_icon} alt="" />
+        <img
+          onClick={() => setExtended(!extended)}
+          className="block w-5 ml-2 cursor-pointer "
+          src={assets.menu_icon}
+          alt=""
+        />
+        <div className="inline-flex mt-12 items-center gap-2 py-2 px-4 bg-blue-200 rounded-3xl text-sm text-gray-500 cursor-pointer ">
+          <img className="w-5" src={assets.plus_icon} alt="" />
+          {extended && <p>New Chat</p>}
+        </div>
+        {extended && (
+          <div className="flex flex-col">
+            <p className="mt-7 mb-5">Recent</p>
+            <div className="flex items-start gap-2 p-2 pr-10 rounded-3xl text-gray-800 cursor-pointer hover:bg-gray-300">
+              <img className="w-5" src={assets.message_icon} alt="" />
+              <p>What is React?</p>
+            </div>
+          </div>
+        )}
       </div>
-      <div className=""></div>
+      <div className="flex flex-col ">
+        <div className="pr-2 cursor-pointer flex items-start gap-2 p-2 rounded-3xl text-gray-800  hover:bg-gray-300">
+          <img className="w-5" src={assets.question_icon} alt="" />
+          {extended && <p className="">Help</p>}
+        </div>
+        <div className="pr-2 cursor-pointer flex items-start gap-2 p-2 rounded-3xl text-gray-800 hover:bg-gray-300">
+          <img className="w-5" src={assets.history_icon} alt="" />
+          {extended && <p className="">Activity</p>}
+        </div>
+        <div className="pr-2 cursor-pointer flex items-start gap-2 p-2 rounded-3xl text-gray-800  hover:bg-gray-300">
+          <img className="w-5" src={assets.setting_icon} alt="" />
+          {extended && <p className="">Settings</p>}
+        </div>
+      </div>
     </div>
   );
 };
