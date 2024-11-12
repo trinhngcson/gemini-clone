b070bd927ad3832347fd308b258c0c4432aeaf2c
Trịnh Ngọc Sơn <77819889+trinhngcson@users.noreply.github.com>
2024-11-12 18:43:35 +0700

    Initial commit

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { assets } from "../../assets/assets";
+const Sidebar = () => {
+  return (
+    <div className="">
+      <div className="">
+        <img className="" src={assets.menu_icon} alt="" />
+      </div>
+      <div className=""></div>
+    </div>
+  );
+};
+
+export default Sidebar;
